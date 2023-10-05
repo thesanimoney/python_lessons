05d2041ac7ff33ff5ed5b71ee2ecea78a4d1d411
thesanimoney <thesanimoney@gmail.com>
2023-10-05 08:06:36 +0300

    try exception method

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("hello1")
+number1 = int(input('Please enter first number'))
+number2 = int(input('Please enter second number'))
+operator = input('Please enter operator')
+
+if '/' in operator:
+    x = number1 / number2
+    print('your result ', x)
+
+elif '*' in operator:
+    x = number1 * number2
+    print('your result ', x)
+
+elif '+' in operator:
+    x = number1 + number2
+    print('your result ', x)
+
+elif '-' in operator:
+    x = number1 - number2
+    print('your result ', x)
--- try_exept.py
+++ try_exept.py
@@ -1,4 +1,12 @@
 try:
-  print(x)
+  f = open("myfile.txt", 'x')
+  try:
+    f.write('My file content bla bla bla')
+  except:
+    print("Something went wrong when writing to the file")
+  finally:
+    f = open('myfile.txt', 'r')
+    print(f.read())
+    f.close()
 except:
-  print("An exception occurred")
+  print("Something went wrong when opening the file")
