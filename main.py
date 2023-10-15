9e5156874c18d84a55eb8b7b417c031b4cd8c448
thesanimoney <thesanimoney@gmail.com>
2023-10-15 16:04:23 +0300

    classses

--- main.py
+++ main.py
@@ -1,19 +1,61 @@
-number1 = int(input('Please enter first number'))
-number2 = int(input('Please enter second number'))
-operator = input('Please enter operator')
+# class Point:
+#     def __init__(self, x, y):
+#         self.x = x
+#         self.y = y
+#
+#     @classmethod
+#     def zero(cls):
+#        return cls(2, 3)
+#
+#     def draw(self):
+#         return f'Cordinate x: {self.x} \nCoordinate y: {self.y}'
+#
+#     def __str__(self):
+#         return f'{self.x} {self.y}'
+#
+#     def __eq__(self, other):
+#         return self.x == other.x and self.y == other.y
+#
+#     def __gt__(self, other):
+#         return self.x > other.x and self.y > other.y
+#
+#     def __add__(self, other):
+#         return Point(self.x + other.x, self.y + other.y)
+#
+#
+# point = Point(9,11)
+# point1 = Point(10,12)
+# print(point1 + point)
 
-if '/' in operator:
-    x = number1 / number2
-    print('your result ', x)
+class TagCloud:
+    def __init__(self):
+        self.tags = {}
 
-elif '*' in operator:
-    x = number1 * number2
-    print('your result ', x)
+    def add(self, tag):
+        self.tags[tag.lower()] = self.tags.get(tag.lower(), 0) + 1
 
-elif '+' in operator:
-    x = number1 + number2
-    print('your result ', x)
+    def __getitem__(self, tag):
+        return self.tags.get(tag.lower(), 0)
 
-elif '-' in operator:
-    x = number1 - number2
-    print('your result ', x)
+    def __setitem__(self, tag, value):
+       self.tags[tag.lower()] = value
+
+    def __len__(self):
+        return len(self.tags)
+
+    def __iter__(self):
+       return iter(self.tags.items())
+
+
+cloud = TagCloud()
+
+cloud.add('Python')
+cloud.add('Python')
+cloud['python'] = 12
+cloud['Jython'] = 12
+cloud.add('Java')
+cloud.add('JavaScript')
+
+for tag, values in cloud:
+    print(values)
+
